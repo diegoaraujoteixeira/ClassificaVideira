f5c945f0a6808e36a2c1053da0acfbe3b5531be9
Diego Araújo Teixeira <130317937+diegoaraujoteixeira@users.noreply.github.com>
2025-07-14 11:39:38 +0000

    app.py criado

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Classifica Folha de Videiras"
+    )
+    st.write("#Classifica Folha de Videiras")
+    #carrega modelo
+
+    #carrega a img
+
+    #classifica a img
+
+    if __name__ == "__main__":
+        main()

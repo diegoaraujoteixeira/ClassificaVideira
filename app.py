2311144c651389fa4c334735a0026f82f6eb76e9
Diego Araújo Teixeira <130317937+diegoaraujoteixeira@users.noreply.github.com>
2025-07-14 12:31:20 +0000

    atualização do codigo

--- app.py
+++ app.py
@@ -1,15 +1,68 @@
 import streamlit as st
+import gdown
+import tensorflow as tf
+import io
+from PIL import Image
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+
+# armazenar em cache pra não precisar baixar sempre que der refresh
+@st.cache_resource
+
+def carrega_modelo():
+    # https://drive.google.com/file/d/1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp/view?usp=sharing
+    url = "https://drive.google.com/uc?id=1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp"
+    gdown.download(url, 'modelo_quantizado16bits.tflite') # baixa o arquivo (modelo)
+
+    # carrega o modelo
+    interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
+
+    # disponibiliza para uso
+    interpreter.allocate_tensors()
+
+    return interpreter
+
+def carrega_imagem():
+    uploaded_file = st.file_uploader("Arraste e solte a imagem ou clique para selecionar uma", type=['png', 'jpg', 'jpeg'])
+
+    if uploaded_file is not None:
+        # ler a imagem
+        image_data = uploaded_file.read()
+
+        # abrir a imagem
+        image = Image.open(io.BytesIO(image_data))
+
+        # exibir a imagem na página
+        st.image(image)
+        st.success("Imagem foi carregada com sucesso")
+
+        # converter a imagem em ponto flutuante
+        image = np.array(image, dtype=np.float32)
+
+        # normalizar a imagem
+        image = image/255.0
+
+        # adicionar uma dimensão extra
+        image = np.expand_dims(image, axis=0)
+        
+        return image
 
 def main():
     st.set_page_config(
-        page_title="Classifica Folha de Videiras"
+        page_title="Classifica folhas de Videiras"
     )
-    st.write("# Classifica Folha de Videiras")
-    #carrega modelo
+    st.write("# Classifica folhas de videiras")
+    # carregar o modelo
+    interpreter = carrega_modelo()
 
-    #carrega a img
 
-    #classifica a img
+    # carregar a imagem
+    image = carrega_imagem()
+
+
+    # classificar a imagem
 
 if __name__ == "__main__":
     main()
